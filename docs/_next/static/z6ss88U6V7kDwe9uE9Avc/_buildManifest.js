860678b8905e980ab97b3cfd42a5d7cd3b37a532
Kit Menke <cwmenke@gmail.com>
2026-02-06 22:23:50 -0600

    Update basePath

--- docs/_next/static/z6ss88U6V7kDwe9uE9Avc/_buildManifest.js
+++ docs/_next/static/z6ss88U6V7kDwe9uE9Avc/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/emoji-dance-party//_next/:path+",
+        "destination": "/emoji-dance-party/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  distDir: "docs",
+  basePath: "/emoji-dance-party",
+  assetPrefix: "/emoji-dance-party/",
 };
 
 export default nextConfig;
+
